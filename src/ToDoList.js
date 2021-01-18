65ee5c3f99ba724fa0dd310d24c70de671885b4a
danudel <danamula@gmail.com>
2021-01-18 10:25:15 +0200

    first component

---
+++ src/ToDoList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+class ToDoList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+      }
+        render() {
+            return (
+                <div>
+                    <h1>to do list</h1>
+                </div>
+            );
+        }
+    
+
+}
+
+
+
+export default ToDoList;
